bb4eea58e8174268df8cbdc591463fa459eca962
DaniSanchezSantolaya <danisbd89@gmail.com>
2023-04-25 08:22:06 +0200

    add pandas pdp tests

---
+++ tests/explainability/test_partial_dependence.py
@@ -0,0 +1,91 @@
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from mercury.explainability import PartialDependenceExplainer
+
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import StringIndexer, VectorAssembler
+from pyspark.sql.functions import when, col
+
+import pytest
+
+pytestmark = pytest.mark.usefixtures("model_and_data_pdp")
+
+def test_pandas_classification(model_and_data_pdp):
+    """ Tests the explainer for  multinomial classification task using "plain Python"
+        models
+    """
+    rf = model_and_data_pdp['rf_iris_sk']
+    features = model_and_data_pdp['iris_pd_df']
+
+    explainer = PartialDependenceExplainer(rf.predict_proba)
+    explanation = explainer.explain(features)
+    return_dict = explanation.data
+
+    # Explanation should contain all the features
+    assert list(return_dict.keys()) == list(features.columns)
+    # Predictions should contain R^3 vectors
+    assert return_dict[list(return_dict.keys())[0]]['preds'].shape[1] == 3
+    # Quantiles should be R^3
+    assert return_dict[list(return_dict.keys())[0]]['lower_quantile'].shape[1] == 3
+    assert return_dict[list(return_dict.keys())[0]]['upper_quantile'].shape[1] == 3
+
+    # Check plotting doesnt crash
+    explanation.plot()
+
+def test_pandas_regression(model_and_data_pdp):
+    rf = model_and_data_pdp['rf_houses_sk']
+    features = model_and_data_pdp['houses_pd_df']
+    explainer = PartialDependenceExplainer(rf.predict, verbose=True)
+
+    features_to_ignore = ['HouseAge', 'AveBedrms', 'Population']
+    features_to_use = [f for f in list(features.columns) if f not in
+            features_to_ignore]
+
+    explanation = explainer.explain(features, ignore_feats=features_to_ignore)
+
+    return_dict = explanation.data
+    # Explanation should contain all the non-ignored features
+    assert list(return_dict.keys()) == features_to_use
+    # Predictions should contain real numbers
+    assert len(return_dict[list(return_dict.keys())[0]]['preds'].shape) == 1
+    # Quantiles should contain real numbers
+    assert len(return_dict[list(return_dict.keys())[0]]['lower_quantile'].shape) == 1
+    assert len(return_dict[list(return_dict.keys())[0]]['upper_quantile'].shape) == 1
+
+    # Check plotting doesnt crash
+    explanation.plot()
+
+
+def test_pandas_regression_with_categoricals(model_and_data_pdp):
+    rf = model_and_data_pdp['rf_boston_sk']
+    features = model_and_data_pdp['boston_pd_df']
+    explainer = PartialDependenceExplainer(rf.predict, quantiles=False)
+    explanation = explainer.explain(features, ignore_feats=['PTRATIO', 'B', 'LSTAT', 'AGE'])
+    return_dict = explanation.data
+    # Predictions should contain real numbers
+    assert len(return_dict[list(return_dict.keys())[0]]['preds'].shape) == 1
+
+    assert len(return_dict['NOX']['values']) == 50
+
+    # Check plotting doesnt crash
+    explanation.plot()
+
+
+def test_explanation_plot(model_and_data_pdp):
+    rf = model_and_data_pdp['rf_boston_sk']
+    features = model_and_data_pdp['boston_pd_df']
+    explainer = PartialDependenceExplainer(rf.predict)
+    explanation = explainer.explain(features, ignore_feats=['PTRATIO', 'B', 'LSTAT', 'AGE'])
+
+    assert len(explanation['CHAS'][0]) == 2 and len(explanation['CHAS'][1]) == 2
+
+    _, ax = plt.subplots()
+    explanation.plot_single('CRIM', ax=ax)
+    assert ax.get_title() == 'CRIM'
+
+    # Check that plotting doesnt crash
+    explanation.plot(quantiles=True)
+
+
